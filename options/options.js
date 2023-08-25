e42f85146dc31e8db502e6a5af7a760a78f55237
Santosh Purbey <purbey.santosh@hotmail.com>
2023-08-25 19:17:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-shape-28",
+  url: "https://royal-shape-28.botics.co",
+  api: "https://royal-shape-28.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
